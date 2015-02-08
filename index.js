0369bde4805b7371792b4ebf405133f6d5ece31b
samhamm <samhamm@gmail.com>
2015-02-07 19:49:38 -0800

    create lib/ and test/ directories; npm init and npm install; Gruntfile

--- index.js
+++ index.js
@@ -4,15 +4,16 @@
 // ---------------------------
 // Gaye Bulut
 // Tricia Buckenberger
-//  Sam Hamm
+// Sam Hamm
 // ---------------------------
 
 'use strict';
 
 var fs = require('fs');
-var bitmap = fs.readFileSync('test.bmp');
+var bitmap = fs.readFileSync('img/test.bmp');
 var bitmapObject = {};
 var HEADER_SIZE = 54;
+var buf = new Buffer(bitmap);
 
 bitmapObject.type = bitmap.toString('utf-8', 0, 2); // reads type
 bitmapObject.size = bitmap.readUInt32LE(2); // reads size
@@ -22,12 +23,10 @@ bitmapObject.height = bitmap.readUInt32LE(22);
 bitmapObject.paletteSize = bitmap.readUInt32LE(46);
 bitmapObject.colorDepth = bitmap.readUInt16LE(28);
 
-console.dir(bitmapObject);
-
-var buf = new Buffer(bitmap);
-
 for (var i = HEADER_SIZE; i < HEADER_SIZE + bitmapObject.paletteSize; i++) {
-  buf[i] = 255 - buf[i];
-};
+      buf[i] = 255 - buf[i];
+}
+
+console.dir(bitmapObject);
 
 fs.writeFile('output.bmp', buf);
